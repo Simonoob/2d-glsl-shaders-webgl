68eb1b9eab319a417b1cb99a9e917d505f80d3f0
simonoob <simonefiorentini1@gmail.com>
2024-01-20 16:02:33 +0200

    chore:cleanup

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -1,16 +1,3 @@
-import { WebGLRenderer, sRGBEncoding } from "three";
-
-const setupEnv = (canvas: HTMLCanvasElement) => {
-  const renderer = new WebGLRenderer({
-    canvas,
-    antialias: true,
-  });
-  renderer.physicallyCorrectLights = true;
-  renderer.outputEncoding = sRGBEncoding;
-  renderer.toneMappingExposure = 1.75;
-  renderer.shadowMap.enabled = true;
-  renderer.setSize(600, 600); //TODO: CHANGE ME
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-};
+const setupEnv = (canvas: HTMLCanvasElement) => {};
 
 export default setupEnv;
