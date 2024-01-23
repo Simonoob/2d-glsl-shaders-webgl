5e405e471b6bd84a3cf209ac5e5a69bc9bab0f25
simonoob <simonefiorentini1@gmail.com>
2024-01-23 09:38:30 +0200

    feat: add mouse uniform

--- src/shaders/fragment.ts
+++ src/shaders/fragment.ts
@@ -4,8 +4,24 @@ varying vec2 vUv;
 
 uniform float u_ratio;
 uniform float u_time;
+uniform vec2 u_pointer;
+
+
 
 void main() {
-    vec2 uv = vUv;
-    gl_FragColor = vec4(vec2(vUv), cos(u_time), 1.0);
+
+    vec2 diffWithDirection = u_pointer - vUv;
+    // adjust for aspect ratio
+    diffWithDirection.x *= u_ratio;
+
+    float distanceFromPointer = length(diffWithDirection);
+
+
+    // limit radius and make value bool (1 or 0)
+    distanceFromPointer = step(0.01, distanceFromPointer + (cos(u_time) * 0.01));
+
+
+    vec3 color = vec3(vUv, distanceFromPointer);
+
+    gl_FragColor = vec4(color, 1.0);
 }`;
